b74e3f6b08e16260f2d767e5e92e3275ca48cd80
Deepanshu Tyagi <60540237+Deepanshu09Tyagi@users.noreply.github.com>
2023-07-13 23:02:14 +0530

    modify userregister

--- controller/app/userController.js
+++ controller/app/userController.js
@@ -1,13 +1,13 @@
 const user = require('../../models/app/userModel');
 
 const userRegister = async (req, res) => {
-    const { phone, email, password, cPassword } = req.body;
-    // if (!phone || !email) {
-    //     return res.send({
-    //         message: "Please provide either phone or email!",
-    //         status: false
-    //     })
-    // }
+    const { registerKey, password, cPassword } = req.body;
+    if (!registerKey) {
+        return res.send({
+            message: "Please provide a key to register with!",
+            status: false
+        })
+    }
     if (!password && !cPassword) {
         return res.send({
             message: "Please provide both password or confirm password!",
@@ -20,19 +20,49 @@ const userRegister = async (req, res) => {
             status: false
         })
     }
-    // const userData = await user.findOne({ $or: [{ phone }, { email }] });
-    // if (userData) {
-    //     return res.send({
-    //         message: `This ${phone || email} is already registered`,
-    //         status: false
-    //     })
-    // }
-    await user.create({ phone, email, password });
+    const userKey = registerKey.includes('@') || registerKey.includes('.');
+    if (userKey) {
+        email = registerKey;
+        if (email.length > process.env.REGISTER_EMAIL_LENGTH) {
+            return res.send({
+                message: "Email must be less than 50 characters!",
+                status: false
+            })
+        }
+        const findwithEmail = await user.findOne({ email });
+        if (findwithEmail) {
+            return res.send({
+                message: "This email is already in use!",
+                status: false
+            })
+        }
+        await user.create({ email, password });
+    } else {
+        phone = registerKey;
+        if (phone.length !== process.env.REGISTER_PHONE_LENGTH) {
+            return res.send({
+                message: "Error in number!",
+                status: false
+            })
+        }
+        const findwithPhone = await user.findOne({ phone });
+        if (findwithPhone) {
+            return res.send({
+                message: "This number is already in use!",
+                status: false
+            })
+        }
+        await user.create({ phone, password });
+    }
     res.send({
         message: "User registered Successfully!",
         status: true
     })
 };
+
+// const userLogin = async (req,res) => {
+//     const {phone, email, }
+// }
 
 module.exports = {
     userRegister
