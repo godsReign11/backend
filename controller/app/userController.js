b1d45a7c3ba6297ae24e17d6092dbad08bdaec4e
Deepanshu Tyagi <60540237+Deepanshu09Tyagi@users.noreply.github.com>
2023-07-13 23:28:45 +0530

    passwordlogin api

--- controller/app/userController.js
+++ controller/app/userController.js
@@ -23,7 +23,7 @@ const userRegister = async (req, res) => {
     const userKey = registerKey.includes('@') || registerKey.includes('.');
     if (userKey) {
         email = registerKey;
-        if (email.length > process.env.REGISTER_EMAIL_LENGTH) {
+        if (email.length > parseInt(process.env.REGISTER_EMAIL_LENGTH)) {
             return res.send({
                 message: "Email must be less than 50 characters!",
                 status: false
@@ -39,7 +39,7 @@ const userRegister = async (req, res) => {
         await user.create({ email, password });
     } else {
         phone = registerKey;
-        if (phone.length !== process.env.REGISTER_PHONE_LENGTH) {
+        if (phone.length !== parseInt(process.env.REGISTER_PHONE_LENGTH)) {
             return res.send({
                 message: "Error in number!",
                 status: false
@@ -60,10 +60,70 @@ const userRegister = async (req, res) => {
     })
 };
 
-// const userLogin = async (req,res) => {
-//     const {phone, email, }
-// }
+const passwordLogin = async (req, res) => {
+    const { loginKey, password } = req.body;
+    if (!loginKey) {
+        return res.send({
+            message: "Please provide a key to login with!",
+            status: false
+        })
+    }
+    if (!password) {
+        return res.send({
+            message: "Please provide password to login!",
+            status: false
+        })
+    }
+    const userKey = loginKey.includes('@') || loginKey.includes('.');
+    if (userKey) {
+        email = loginKey;
+        if (email.length > parseInt(process.env.REGISTER_EMAIL_LENGTH)) {
+            return res.send({
+                message: "Email must be less than 50 characters!",
+                status: false
+            })
+        }
+        const findwithEmail = await user.findOne({ email });
+        if (!findwithEmail) {
+            return res.send({
+                message: "This email doesn't exist!",
+                status: false
+            })
+        }
+        if (findwithEmail.password !== password) {
+            return res.send({
+                message: "Password doesn't match!",
+                status: false
+            })
+        }
+    } else {
+        phone = loginKey;
+        if (phone.length !== parseInt(process.env.REGISTER_PHONE_LENGTH)) {
+            return res.send({
+                message: "Error in number!",
+                status: false
+            })
+        }
+        const findwithPhone = await user.findOne({ phone });
+        if (!findwithPhone) {
+            return res.send({
+                message: "This number doesn't exist!",
+                status: false
+            })
+        }
+        if (findwithPhone.password !== password) {
+            return res.send({
+                message: "Password doesn't match!",
+                status: false
+            })
+        }
+    }
+    res.send({
+        message: "User login Approved!",
+        status: true
+    })
+}
 
 module.exports = {
-    userRegister
+    userRegister, passwordLogin
 }
--- routes/app/appRoutes.js
+++ routes/app/appRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userController = require('../../controller/app/userController');
 
 router.post('/userRegister', userController.userRegister);
+router.post('/passwordLogin', userController.passwordLogin);
 
 module.exports = router;
