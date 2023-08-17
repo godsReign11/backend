471d9df9c777d5cc094f680ba4adc065c64d4acc
DESKTOP-UUIAGJN\dell <abhishektyagi6230@gmail.com>
2023-08-17 22:35:32 +0530

    new api

--- controller/app/userActivity.js
+++ controller/app/userActivity.js
@@ -1,8 +1,8 @@
 const downloads = require('../../models/app/download')
+const watchlist = require('../../models/app/watchlist')
 
 var storeUserDownloads = async function(req,res)
 {
-    console.log("inn")
     const {videoId,userId,path,thumbnail,title}=req.body;
     await downloads.create({videoId,userId,path,thumbnail,title})
     return res.send({
@@ -14,6 +14,26 @@ var storeUserDownloads = async function(req,res)
 var getUserDownloads = async function(req,res)
 {
     const getData = await downloads.find({userId:req.query.userId,isDeleted:false});
+    return res.send({
+        status:true,
+        message:"success",
+        data:getData
+    })
+}
+
+const storeUserWatchlist = async function(req,res)
+{
+    const {videoId,userId,path,thumbnail,title}=req.body;
+    await watchlist.create({videoId,userId,path,thumbnail,title})
+    return res.send({
+        message: "success",
+        status: true
+    })
+}
+
+var getUserWatchlist = async function(req,res)
+{
+    const getData = await watchlist.find({userId:req.query.userId,isDeleted:false});
     return res.send({
         status:true,
         message:"success",
